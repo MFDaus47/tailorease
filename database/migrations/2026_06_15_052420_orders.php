babdd829022452aa2cd95c93ae687670cb207dc1
mfdaus47 <mfdaus3158@gmail.com>
2026-06-16 23:04:15 +0800

    added orders table

--- database/migrations/2026_06_15_052420_orders.php
+++ database/migrations/2026_06_15_052420_orders.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreign('customer_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreignId('customer_id')->constrained('users')->onDelete('cascade');
         });
     }
 
